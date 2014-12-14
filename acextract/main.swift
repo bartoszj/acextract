6df5eac4fb6eaf5165664a036ecf5358da8facb8
Bartosz Janda <bartosz.janda@gmail.com>
2014-12-14 17:01:37 +0100

    Initial Commit

---
+++ acextract/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  acextract
+//
+//  Created by Bartosz Janda on 14.12.2014.
+//  Copyright (c) 2014 Bartosz Janda. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
