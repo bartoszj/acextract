4362a6ba76e73d0f52c0a780fb779fdf976c0727
Bartosz Janda <bartosz.janda@gmail.com>
2014-12-22 22:20:45 +0100

    Added example assets.

---
+++ MacTestApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTestApp
+//
+//  Created by Bartosz Janda on 22.12.2014.
+//  Copyright (c) 2014 Bartosz Janda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
